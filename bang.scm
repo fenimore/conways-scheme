25515ae48addc22c07a4c500d25caa01052ce569
polypmer <exorable.ludos@gmail.com>
2016-10-25 11:42:58 -0400

    Begin doing maze

--- bang.scm
+++ bang.scm
@@ -3,20 +3,57 @@
 (require 2htdp/universe 2htdp/image)
 
 
-(define HEIGHT 300)     ; cells
-(define WIDTH 300)
+; rendering functions
+; world->scene: World -> Image
 
-(define ufo (square 50 "solid" "darkmagenta"))
+(struct node (visited doors x y) #:transparent)
+
+(define ROWS 30)     ; cells
+(define COLS 30)
+(define CELL-SIZE 10)
+(define BACKGROUND (empty-scene (* COLS CELL-SIZE)
+                                (* ROWS CELL-SIZE)))
+
+(define-struct world (maze))
+
+;; NODES
+(define n1 (node #f 0 0 0)) ;; origin
+(define n2 (node #f 0 0 10)) ;; above
+(define n3 (node #f 0 10 0)) ;; right of
+(define n4 (node #f 0 10 10)) ;; Diagnol above
+(define maze-world ;; The GAME WORLD
+  (make-world
+   (list n1 n2 n3 n4)))
+
+
+(define ufo (square 30 "solid" "darkmagenta"))
 
 (define (add-3-to-state current-state)
   (+ current-state 3))
 
 
-(define (draw-a-ufo-onto-an-empty-scene current-state)
-  (place-image ufo (/ WIDTH 2) current-state
-               (empty-scene WIDTH HEIGHT)))
+(define (draw-onto-grid img x y bkg)
+  (place-image
+   img
+   (* CELL-SIZE x)
+   (* CELL-SIZE (- y ROWS))
+   bkg))
+
+;; place-image takes image coords and background
+(define (draw-maze w)
+  (draw-onto-grid ufo 100 100
+                  (draw-onto-grid ufo 10 10 BACKGROUND)))
+
+;;(node-visited (third nodes))
+;;(node-visited n1)
+(define (render w)
+  ;;
+  (draw-onto-grid ufo
+                  (node-x (second (world-maze maze-world)))
+                  (node-y (second (world-maze maze-world)))
+                  BACKGROUND))
 
 ;; RENDER
-(big-bang 0
-          (on-tick add-3-to-state)
-          (to-draw draw-a-ufo-onto-an-empty-scene))
+(big-bang maze-world
+          ;;(on-tick add-3-to-state)
+          (to-draw render))
