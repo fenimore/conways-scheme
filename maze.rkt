47bd83d15722ed2d6e141fad3f33e816b64b5e5c
polypmer <exorable.ludos@gmail.com>
2016-10-25 17:04:06 -0400

    TThanks @alex-segura

--- maze.rkt
+++ maze.rkt
@@ -26,13 +26,16 @@
 (struct node (visited x y img) #:transparent)
 (define maze '())
 
+
 (define (make-board)
   "A Vector of Vectors for the grid"
-  (build-vector rows
-                (lambda (m) ;; m is the y coord
-                  (build-vector cols
-                                (lambda (n) ;; n is the x coord
-                                  (node #f m n (empty-block)))))))
+  (vector->list
+   (build-vector rows
+                 (lambda (m) ;; m is the y coord
+                   (vector->list
+                    (build-vector cols
+                                  (lambda (n) ;; n is the x coord
+                                    (node #f m n (empty-block)))))))))
 
 ;; depricated
 (define (draw-onto-grid img x y bkg)
@@ -50,19 +53,13 @@
    (- (- (* cell (node-y n)) (- (* cell cols) margin)))
    bkg))
 
-(define (draw-nodes node-vec bkg)
-  (if
-   (= (vector-length node-vec) 1)
-   (draw-node
-    (vector-ref node-vec 0) bkg)
-   (draw-nodes
-    (vector-drop node-vec 1)
-    (draw-node (vector-ref node-vec 0) bkg))))
-
 (define (draw-col nodes background)
   (foldl draw-node background nodes))
 
-;; Step One: Loop through rows
+(define (draw-nodes nodes)
+  (foldl draw-col background nodes))
+
+;; Step One: Loop through rws
 ;; In each row, loop through columns
 ;; For each node, call draw-onto-grid
 ;;(for ([i (make-board)])
@@ -72,9 +69,8 @@
 (define (render w)
   "game state is maze, list of nodes"
 ;;  (draw-node
-;;   (vector-ref (vector-ref (make-board) 0) 0) background))
-  (draw-col
-   (vector->list (vector-ref (make-board) 0)) background))
+  ;;   (vector-ref (vector-ref (make-board) 0) 0) background))
+  (draw-nodes (make-board)))
   ;;(for ([row (make-board)])
   ;;(draw-nodes row background)))
 
