333cc71ca7ade1baecb101fba2d4cebe7c0aca8f
polypmer <exorable.ludos@gmail.com>
2016-10-25 16:37:00 -0400

    Draw with nodes

--- maze.rkt
+++ maze.rkt
@@ -13,22 +13,28 @@
                     (* cols cell)
                     (* rows cell)))
 
+;;;;;;;;;;
+;; Blocks
 (define (empty-block)
   (square cell "outline" "black"))
 ;;(square CELL-SIZE #:outline #:black))
-
 ;;(define full-block
   ;;(square cell #:solid #:black))
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Structs
 (struct node (visited x y img) #:transparent)
+(define maze '())
 
 (define (make-board)
+  "A Vector of Vectors for the grid"
   (build-vector rows
-                (lambda (m)
+                (lambda (m) ;; m is the y coord
                   (build-vector cols
-                                (lambda (n)
+                                (lambda (n) ;; n is the x coord
                                   (node #f m n (empty-block)))))))
 
+;; depricated
 (define (draw-onto-grid img x y bkg)
   "Wrapper for place-image"
   (place-image
@@ -38,15 +44,42 @@
    bkg))
 
 (define (draw-node n bkg)
-  (node-img n)
-  (node-x n)
-  (node-y n)
-  bkg)
+  (place-image
+   (node-img n)
+   (+ (* cell (node-x n)) margin)
+   (- (- (* cell (node-y n)) (- (* cell cols) margin)))
+   bkg))
+
+(define (draw-nodes node-vec bkg)
+  (if
+   (= (vector-length node-vec) 1)
+   (draw-node
+    (vector-ref node-vec 0) bkg)
+   (draw-nodes
+    (vector-drop node-vec 1)
+    (draw-node (vector-ref node-vec 0) bkg))))
+
+
 
 ;; Step One: Loop through rows
 ;; In each row, loop through columns
 ;; For each node, call draw-onto-grid
-(for ([i (make-board)])
-  (for ([j i])
-    (display j)
-    (newline)))
+;;(for ([i (make-board)])
+;;  (for ([j i])
+;    (display j)))
+
+
+
+
+
+(define (render w)
+  "game state is maze, list of nodes"
+  (draw-node
+   (vector-ref (vector-ref (make-board) 0) 0) background))
+  ;;(for ([row (make-board)])
+  ;;(draw-nodes row background)))
+
+
+
+(big-bang maze
+          (to-draw render))
