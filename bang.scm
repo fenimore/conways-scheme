25fa777f9ccc14bb901d5f41723a2340acf4d1db
polypmer <exorable.ludos@gmail.com>
2016-10-25 14:03:45 -0400

    RRender all nodes recursively WIP

--- bang.scm
+++ bang.scm
@@ -6,62 +6,101 @@
 ; rendering functions
 ; world->scene: World -> Image
 
-(struct node (visited x y) #:transparent)
+(struct node (visited x y img) #:transparent)
 
-(define ROWS 30)     ; cells
-(define COLS 30)
-(define CELL-SIZE 10)
+(define ROWS 10)     ; cells
+(define COLS 10)
+(define CELL-SIZE 30)
+(define MARGIN (/ CELL-SIZE 2))
 (define BACKGROUND (empty-scene (* COLS CELL-SIZE)
                                 (* ROWS CELL-SIZE)))
 
 (define-struct world (maze))
 
+(define blue-block (square 30 "solid" "blue"))
+(define green-block (square 30 "solid" "green"))
+(define red-block (square 30 "solid" "red"))
+(define yellow-block (square 30 "solid" "yellow"))
+
+
 ;; NODES
-(define n1 (node #f 0 0)) ;; origin
-(define n2 (node #f 0 1)) ;; above
-(define n3 (node #f 1 0)) ;; right of ;; green
-(define n4 (node #f 3 1)) ;; Diagnol above ;; blue
+(define n1 (node #f 0 0 blue-block)) ;; origin
+(define n2 (node #f 0 1 green-block)) ;; above
+(define n3 (node #f 1 0 red-block)) ;; right of ;; green
+(define n4 (node #f 1 1 yellow-block)) ;; Diagnol above ;; blue
 (define maze-world ;; The GAME WORLD
   (make-world
    (list n1 n2 n3 n4)))
 
 
-(define blue-block (square 10 "solid" "blue"))
-(define green-block (square 10 "solid" "green"))
-(define red-block (square 10 "solid" "red"))
-(define yellow-block (square 10 "solid" "yellow"))
 
 
 
 (define (draw-onto-grid img x y bkg)
   (place-image
    img
-   (+ (* CELL-SIZE x) 5)
-   (- (- (* CELL-SIZE y) 295))
+   (+ (* CELL-SIZE x) MARGIN)
+   (- (- (* CELL-SIZE y) (- (* CELL-SIZE COLS) MARGIN)))
    bkg))
 
 
+(define (d-node n bkg)
+  (draw-onto-grid
+   (node-img n)
+   (node-x n)
+   (node-y n)
+   (bkg)))
+
+(define (d-nodes node-lst bkg)
+  (if
+   (= (length node-lst) 1)
+   (d-node (car node-lst) bkg)
+   (d-nodes (drop node-lst 1) (d-node (car node-lst) bkg))
+   )
+  )
+
+(define (test-render w)
+  (draw-onto-grid
+   (node-img (car (world-maze w)))
+   (node-x (car (world-maze w)))
+   (node-y (car (world-maze w)))
+   BACKGROUND)
+  )
+
+(define (render w)
+  (test-nodes (world-maze w) BACKGROUND))
+
+(define (test-nodes node-lst bkg)
+  (draw-onto-grid
+   (node-img (car node-lst))
+   (node-x (car node-lst))
+   (node-y (car node-lst))
+   bkg)
+  )
+  ;;(draw-node (car (world-maze maze-world)) BACKGROUND))
+  ;;(draw-nodes (world-maze w) BACKGROUND))
+
 ;;(node-visited (third nodes))
 ;;(node-visited n1)
-(define (render w)
+(define (depricated-render w)
   ;;
   (draw-onto-grid
-   yellow-block
+   (node-img (first (world-maze maze-world)))
    (node-x (first (world-maze maze-world)))
    (node-y (first (world-maze maze-world)))
 
    (draw-onto-grid
-    red-block
+    (node-img (second (world-maze maze-world)))
     (node-x (second (world-maze maze-world)))
     (node-y (second (world-maze maze-world)))
 
     (draw-onto-grid
-     green-block
+     (node-img (third (world-maze maze-world)))
      (node-x (third (world-maze maze-world)))
      (node-y (third (world-maze maze-world)))
 
      (draw-onto-grid
-      blue-block
+      (node-img (fourth (world-maze maze-world)))
       (node-x (fourth (world-maze maze-world)))
       (node-y (fourth (world-maze maze-world)))
       BACKGROUND)))))
