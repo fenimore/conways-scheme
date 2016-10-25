dd8a62585ba3a04e1aa928589499a8a10a48958c
polypmer <exorable.ludos@gmail.com>
2016-10-25 16:04:24 -0400

    Print the nodes

--- maze.rkt
+++ maze.rkt
@@ -13,26 +13,40 @@
                     (* cols cell)
                     (* rows cell)))
 
-(define empty-block
-  (square CELL-SIZE #:outline #:black))
+(define (empty-block)
+  (square cell "outline" "black"))
+;;(square CELL-SIZE #:outline #:black))
 
-(define full-block
-  (square CELL-SIZE #:solid #:black))
+;;(define full-block
+  ;;(square cell #:solid #:black))
 
-
-;; Rendering World
-;;(define world
-
-(struct node (visited x y img))
-
-
+(struct node (visited x y img) #:transparent)
 
 (define (make-board)
   (build-vector rows
                 (lambda (m)
                   (build-vector cols
                                 (lambda (n)
-                                  (node #f m n empty-block))))))
+                                  (node #f m n (empty-block)))))))
 
-(define (render board)
-  )
+(define (draw-onto-grid img x y bkg)
+  "Wrapper for place-image"
+  (place-image
+   img
+   (+ (* cell x) margin)
+   (- (- (* cell y) (- (* cell cols) margin)))
+   bkg))
+
+(define (draw-node n bkg)
+  (node-img n)
+  (node-x n)
+  (node-y n)
+  bkg)
+
+;; Step One: Loop through rows
+;; In each row, loop through columns
+;; For each node, call draw-onto-grid
+(for ([i (make-board)])
+  (for ([j i])
+    (display j)
+    (newline)))
