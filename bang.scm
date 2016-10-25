23d90d0964ec0fbe84e056fffeca58e5b05420bb
polypmer <exorable.ludos@gmail.com>
2016-10-25 12:18:53 -0400

    Grrid up and running

--- bang.scm
+++ bang.scm
@@ -6,7 +6,7 @@
 ; rendering functions
 ; world->scene: World -> Image
 
-(struct node (visited doors x y) #:transparent)
+(struct node (visited x y) #:transparent)
 
 (define ROWS 30)     ; cells
 (define COLS 30)
@@ -17,41 +17,54 @@
 (define-struct world (maze))
 
 ;; NODES
-(define n1 (node #f 0 0 0)) ;; origin
-(define n2 (node #f 0 0 10)) ;; above
-(define n3 (node #f 0 10 0)) ;; right of
-(define n4 (node #f 0 10 10)) ;; Diagnol above
+(define n1 (node #f 0 0)) ;; origin
+(define n2 (node #f 0 1)) ;; above
+(define n3 (node #f 1 0)) ;; right of ;; green
+(define n4 (node #f 3 1)) ;; Diagnol above ;; blue
 (define maze-world ;; The GAME WORLD
   (make-world
    (list n1 n2 n3 n4)))
 
 
-(define ufo (square 30 "solid" "darkmagenta"))
+(define blue-block (square 10 "solid" "blue"))
+(define green-block (square 10 "solid" "green"))
+(define red-block (square 10 "solid" "red"))
+(define yellow-block (square 10 "solid" "yellow"))
 
-(define (add-3-to-state current-state)
-  (+ current-state 3))
 
 
 (define (draw-onto-grid img x y bkg)
   (place-image
    img
-   (* CELL-SIZE x)
-   (* CELL-SIZE (- y ROWS))
+   (+ (* CELL-SIZE x) 5)
+   (- (- (* CELL-SIZE y) 295))
    bkg))
 
-;; place-image takes image coords and background
-(define (draw-maze w)
-  (draw-onto-grid ufo 100 100
-                  (draw-onto-grid ufo 10 10 BACKGROUND)))
 
 ;;(node-visited (third nodes))
 ;;(node-visited n1)
 (define (render w)
   ;;
-  (draw-onto-grid ufo
-                  (node-x (second (world-maze maze-world)))
-                  (node-y (second (world-maze maze-world)))
-                  BACKGROUND))
+  (draw-onto-grid
+   yellow-block
+   (node-x (first (world-maze maze-world)))
+   (node-y (first (world-maze maze-world)))
+
+   (draw-onto-grid
+    red-block
+    (node-x (second (world-maze maze-world)))
+    (node-y (second (world-maze maze-world)))
+
+    (draw-onto-grid
+     green-block
+     (node-x (third (world-maze maze-world)))
+     (node-y (third (world-maze maze-world)))
+
+     (draw-onto-grid
+      blue-block
+      (node-x (fourth (world-maze maze-world)))
+      (node-y (fourth (world-maze maze-world)))
+      BACKGROUND)))))
 
 ;; RENDER
 (big-bang maze-world
